7ceb4ee03adcc681ad22bf646b198441589bb368
fowler013 <fowler0613@gmail.com>
2018-05-24 11:21:41 -0500

    es6 lab finished

---
+++ es6.js
@@ -0,0 +1,41 @@
+let name = 'Tevin';
+function myFunc(){
+let spread = [...name];
+console.log(spread)
+}myFunc();
+
+
+
+
+
+
+
+
+
+
+
+
+// function aboutMe(name, location, favFood){
+//     console.log(`my name is ${name},i was rasied in  ${location}, and I love ${favFood}`)
+// }
+// let arry = ['Tevin', 'Homewood', 'Seafood'];
+
+// aboutMe(...arry);
+
+
+
+
+
+
+
+
+
+// let me = {
+//     favMovie: 'Fight Club',
+//     getFirstName: 'Tevin'
+// };
+
+// let aboutMe = person => `my name is ${person.getFirstName}, ${person.favMovie}`;
+
+// let result = aboutMe(me);
+// console.log(result);
